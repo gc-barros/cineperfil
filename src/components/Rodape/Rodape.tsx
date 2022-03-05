f4a637277cfccf7af8f7561b14675f95da5c1d63
Gabriel Barros <super.gcb18@gmail.com>
2022-03-05 12:08:02 -0300

    Remodelagem do rodapé e ajustes de responsividade

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -4,7 +4,10 @@ export default function Rodape() {
 
   return (
     <footer className={styles.footer}>
-      <strong>© 2021 Cineperfil.</strong> Todos os direitos reservados.
+      ⚡ Desenvolvido por: 
+      <a href="https://www.linkedin.com/in/faustinoneto/" rel="noreferrer" target="_blank"> Faustino Neto</a>,
+      <a href="https://www.barrosdev.com.br/" rel="noreferrer" target="_blank"> Gabriel Barros</a> &
+      <a href="https://github.com/thiagooliveiraaires" rel="noreferrer" target="_blank"> Thiago Aires</a>.
     </footer>
   );
 }
