e5f76bace2ed450f10f66e14dc51640471e47b78
Gabriel Barros <super.gcb18@gmail.com>
2022-03-04 22:37:56 -0300

    Base da página de cadastro

--- src/pages/Cadastro/Cadastro.tsx
+++ src/pages/Cadastro/Cadastro.tsx
@@ -1,10 +1,67 @@
 import CabecalhoInicial from "components/CabecalhoInicial";
+import styles from './Cadastro.module.scss';
+import Rodape from "components/Rodape";
+import Botao from "components/Botao";
+import Input from "components/Input";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Cadastro() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [senhaConfirmacao, setSenhaConfirmacao] = useState("");
+
   return (
-    <main>
+    <section className={styles.principal}>
       <CabecalhoInicial />
-      <h1>Cadastro</h1>
-    </main>
+      <section className={styles.container}>
+        <form
+          className={styles.form}
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(`Nome: ${nome}. E-mail: ${email}. Senha: ${senha}. Confirmação da senha: ${senhaConfirmacao}`);
+          }}
+        >
+          <p className={styles.subtitulo}>Rápido, fácil e gratuito!</p>
+          <h1 className={styles.titulo}>Seja um cinéfilo</h1>
+          <Input
+            label="Nome"
+            type="text"
+            onChange={(e) => {
+              setNome(e.target.value);
+            }}
+          />
+          <Input
+            label="E-mail"
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <Input
+            label="Senha"
+            type="password"
+            showIcon={true}
+            onChange={(e) => {
+              setSenha(e.target.value);
+            }}
+          />
+          <Input
+            label="Confirmar senha"
+            type="password"
+            showIcon={true}
+            onChange={(e) => {
+              setSenhaConfirmacao(e.target.value);
+            }}
+          />
+          <Botao>Criar conta</Botao>
+          <span className={styles.criarConta}>
+            Já possui uma conta? <Link to="/login">Faça login</Link>
+          </span>
+        </form>
+      </section>
+      <Rodape />
+    </section>
   );
 }
